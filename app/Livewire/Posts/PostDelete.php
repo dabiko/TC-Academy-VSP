10d2b3b068930b72bf5bf7f11b79052e35e7d29b
dabiko <dabiko.blaise.com>
2024-06-03 17:52:39 +0100

    CRUD on post: delete fix

--- app/Livewire/Posts/PostDelete.php
+++ app/Livewire/Posts/PostDelete.php
@@ -2,11 +2,49 @@
 
 namespace App\Livewire\Posts;
 
+use App\Models\Post;
+use App\Traits\EncryptDecrypt;
+use Illuminate\View\View;
+use Livewire\Attributes\Locked;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class PostDelete extends Component
 {
-    public function render()
+
+    use EncryptDecrypt;
+
+    #[Locked]
+    public string $id;
+
+    #[Locked]
+    public string $name;
+
+    public bool $DeletePostModal = false;
+
+    #[On('dispatch-delete-post')]
+    public function set_post($id, $name): void
+    {
+        $this->id = $id;
+        $this->name = $name;
+
+        $this->DeletePostModal  = true;
+    }
+
+     public function deletePost(): void
+    {
+        $delete = Post::destroy($this->decryptId($this->id));
+
+        ($delete)
+            ? $this->dispatch('notify', title: 'success', message:  ' '.$this->name. ' Deleted successfully')
+            : $this->dispatch('notify', title: 'fail', message: 'Ops!! Something went wrong');
+
+        $this->dispatch('dispatch-post-deleted')->to(PostTable::class);
+
+        $this->DeletePostModal  = false;
+
+    }
+    public function render(): View
     {
         return view('livewire.posts.post-delete');
     }
--- resources/views/livewire/posts/post-delete.blade.php
+++ resources/views/livewire/posts/post-delete.blade.php
@@ -1,3 +1,27 @@
 <div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+    <x-dialog-modal wire:model.live="DeletePostModal" submit="edit">
+        <x-slot name="title" class="text-justify">
+            Delete {{ $name }}
+        </x-slot>
+
+        <x-slot name="content">
+            <p>
+                Are you sure you want to delete this branch?
+                Once your account is deleted, all of its resources and data will be permanently deleted.
+            </p>
+        </x-slot>
+
+        <x-slot name="footer">
+            <x-secondary-button @click="$wire.set('DeletePostModal', false)" wire:loading.attr="disabled">
+                <i class="fa-solid fa-circle-xmark fa-xl"></i> &ensp;
+                {{ __('Cancel') }}
+            </x-secondary-button>
+
+            <x-danger-button wire:click="deletePost" class="ml-3 hover:bg-red-700 bg-red-500" wire:loading.attr="disabled">
+                <i class="fa-regular fa-trash-can fa-xl"></i> &ensp;
+                {{ __('Delete') }}
+            </x-danger-button>
+        </x-slot>
+    </x-dialog-modal>
+
 </div>
